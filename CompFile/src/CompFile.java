ddc2671f49e4b667e6992c5bf83f6c521a26bd73
HolyFat <p.s.smoke@gmail.com>
2016-09-09 19:40:16 +0300

    I/O try :D

--- CompFile/src/CompFile.java
+++ CompFile/src/CompFile.java
@@ -3,29 +3,28 @@ import java.io.*;
 /**
  * Created by user on 09.09.16.
  */
- class CompFile {
+class CompFile {
     public static void main(String[] args) throws IOException {
 
 
-       try {
-           File fin = new File("D:/text.txt");
-           BufferedReader f = new BufferedReader(new FileReader(fin));
-           File fin1 = new File("D:/text2.txt");
-           BufferedReader f1 = new BufferedReader(new FileReader(fin1));
+        try {
+            File fin = new File("D:/text.txt");
+            BufferedReader f = new BufferedReader(new FileReader(fin));
+            File fin1 = new File("D:/text2.txt");
+            BufferedReader f1 = new BufferedReader(new FileReader(fin1));
             String line;
-           String line2;
-           line = f.readLine();
-           line2 = f1.readLine();
+            String line2;
+            line = f.readLine();
+            line2 = f1.readLine();
 
-           if (line.equals(line2) ) {
-               System.out.println("Содержимое файлов одинаковое");
-           } else {
-               System.out.println("Содержимое файлов разное");
-           }
+            if (line.equals(line2)) {
+                System.out.println("Содержимое файлов одинаковое");
+            } else {
+                System.out.println("Содержимое файлов разное");
+            }
 
-       }
-       catch(FileNotFoundException ex){
-           System.out.println("Файлы не найдены");
-       }
+        } catch (FileNotFoundException ex) {
+            System.out.println("Файлы не найдены");
+        }
     }
 }
