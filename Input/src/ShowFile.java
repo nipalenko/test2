12fb68e13b44d5338e873589e1dfc569207a2494
HolyFat <p.s.smoke@gmail.com>
2016-09-09 18:55:10 +0300

    I/O try :D

--- Input/src/ShowFile.java
+++ Input/src/ShowFile.java
@@ -1,9 +1,18 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 
 public class ShowFile {
     public static void main(String[] args) throws Exception {
+
+        FileWriter write = new FileWriter("D:/text.txt",true);
+        String text = "\nI fuck english language ";
+        write.write(text);
+
+        write.close();
+
+
 
         File fin = new File("D:/text.txt");
         BufferedReader f = new BufferedReader(new FileReader(fin));
